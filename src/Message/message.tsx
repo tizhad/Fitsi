11bd3ee9d20e96863f388818aff72aa1f4f3532b
tina <tinarezanezhaad@gmail.com>
2023-10-31 18:30:43 +0100

    Modify Message component to show the correct message.

--- src/Message/message.tsx
+++ src/Message/message.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Message() {
+function Message(props: any) {
     return (
         <div>
+            {props.errorMessage && (
+                <div className="alert alert-danger">{props.errorMessage}</div>
+            )}
+            {props.message && (
+                <div className="alert alert-success">{props.message}</div>
+            )}
             <button className='btn btn-primary'>Hey</button>
         </div>
     );
 }
+
 export default Message;
