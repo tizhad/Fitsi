8507261afb7b352d745c5461f6078244bb1021b1
tina <tinarezanezhaad@gmail.com>
2023-10-31 18:13:47 +0100

    New component created for new Component.

---
+++ src/Message/message.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Message() {
+    return (
+        <div>
+            <button className='btn btn-primary'>Hey</button>
+        </div>
+    );
+}
+export default Message;
