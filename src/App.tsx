942cd64c8cd5660c08912eeb742aea225586b4eb
tina <tinarezanezhaad@gmail.com>
2023-10-31 17:52:21 +0100

    Get location when application starts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Location} from "./Models/Location";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [location, setLocation] = useState(new Location());
+    useEffect(() => {
+        getUserCurrentLocation();
+    }, []);
+
+    function getUserCurrentLocation() {
+        navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+            setLocation({
+                ...location,
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+            })
+        })
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Lat: {location.lat} <br/> Long: {location.long}
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
