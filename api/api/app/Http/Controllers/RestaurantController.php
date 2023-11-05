c251a994427b8e734f83c2a757d278a1ac056047
tina <tinarezanezhaad@gmail.com>
2023-11-05 15:23:51 +0100

    New models added for reservations and restaurant.

---
+++ api/api/app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index(): \Illuminate\Database\Eloquent\Collection
+    {
+        return Restaurant::all();
+    }
+
+    public function store(Request $request)
+    {
+        $restaurant = Restaurant::create($request->all());
+
+        return response()->json($restaurant, 201);
+    }
+
+    public function update(Request $request, Restaurant $restaurant)
+    {
+        $restaurant->update($request->all());
+
+        return response()->json($restaurant, 200);
+    }
+
+    public function delete(Restaurant $restaurant)
+    {
+        $restaurant->delete();
+
+        return response()->json(null, 204);
+    }
+
+}
---
+++ api/api/database/seeders/ReservationsTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReservationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('reservations')->insert([
+            [
+                'restaurant_id' => 1,
+                'table_number' => 1,
+                'date' => '2023-12-01',
+                'time' => '18:00:00',
+                'people' => 4,
+            ],
+            [
+                'restaurant_id' => 1,
+                'table_number' => 2,
+                'date' => '2023-12-02',
+                'time' => '19:30:00',
+                'people' => 3,
+            ],
+            [
+                'restaurant_id' => 2,
+                'table_number' => 3,
+                'date' => '2023-12-03',
+                'time' => '20:00:00',
+                'people' => 6,
+            ],
+            [
+                'restaurant_id' => 2,
+                'table_number' => 1,
+                'date' => '2023-12-04',
+                'time' => '17:45:00',
+                'people' => 2,
+            ],
+        ]);
+    }
+}
