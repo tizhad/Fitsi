4b66d6e455522a01028172f6d19ae385afc6340b
tina <tinarezanezhaad@gmail.com>
2023-11-05 15:23:35 +0100

    New endpoints added for reservations and restaurant.

--- api/api/routes/api.php
+++ api/api/routes/api.php
@@ -3,19 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/sample-endpoint', 'SampleController@sampleMethod');
+
+//reservations
+Route::post('/reservations', [\App\Http\Controllers\ReservationController::class, 'store']);
+Route::get('/reservations', [\App\Http\Controllers\ReservationController::class, 'index']);
+Route::put('reservations/{id}', 'ReservationController@update');
+Route::delete('reservations/{id}', 'ReservationController@delete');
+
+//restaurants
+Route::post('/restaurants', [\App\Http\Controllers\RestaurantController::class, 'store']);
+Route::get('/restaurants', [\App\Http\Controllers\ReservationController::class, 'index']);
+Route::get('/restaurants/{id}', [\App\Http\Controllers\ReservationController::class, 'update']);
+Route::get('/restaurants/{id}', [\App\Http\Controllers\ReservationController::class, 'delete']);
