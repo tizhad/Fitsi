9664bbe6248f3df2ad55d409d01c8d5fd057f1ea
tina <tinarezanezhaad@gmail.com>
2023-11-05 19:00:50 +0100

    New model created for restaurant and reservation.

---
+++ View/src/Models/Reservation.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+    id:number;
+    user: string;
+    restaurantId: number;
+    tableNumber: number;
+    date: string;
+    time: string;
+    people: number;
+}
---
+++ View/src/Models/Restaurant.ts
@@ -0,0 +1,7 @@
+export interface Restaurant {
+    id: number;
+    name:string;
+    numberOfTables: number;
+    location: string;
+    status: string;
+}
