183a627dfcbc57e1473d87322ecc18decf405d80
tina <tinarezanezhaad@gmail.com>
2023-11-08 22:02:43 +0100

    Created new component for reservation detail.

---
+++ View/src/components/Reservation/ReservationDetail.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ReservationDetail from './ReservationDetail';
+
+describe('<Reservation />', () => {
+  // test('it should mount', () => {
+  //   render(<ReservationDetail />);
+  //
+  //   const reservation = screen.getByTestId('Reservation');
+  //
+  //   expect(reservation).toBeInTheDocument();
+  // });
+});
---
+++ View/src/components/Reservation/ReservationDetail.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Reservation } from "../../Models/Reservation";
+import { getRestaurant } from "../../Services/reservationService";
+import { Restaurant } from "../../Models/Restaurant";
+
+interface ReservationDetailProps {
+    reservation: Reservation;
+}
+
+function ReservationDetail({ reservation }: ReservationDetailProps) {
+    const [restaurant, setRestaurant] = useState<Restaurant | undefined>();
+
+
+    useEffect(() => {
+        getRestaurantDetail();
+    }, [reservation.id])
+
+    const getRestaurantDetail = async () => {
+        console.log('getRestaurantDetail Fire')
+        const response = await getRestaurant(reservation.restaurant_id);
+        setRestaurant(response);
+        console.log('restaurant', restaurant)
+    }
+
+
+    return (
+        <div className="reservation">
+            <h2>Your Reservation Details</h2>
+            <p>You reserved a table for <br/><strong className="restaurant-name mt-2"> {restaurant?.name}</strong></p>
+            <p><strong>Date:</strong> {reservation.date}</p>
+            <p><strong>For </strong>{reservation.people} people</p>
+            <h6><strong>Your Table number is: </strong>{reservation.table_number}</h6>
+            <h6><strong>At </strong>{reservation.time}</h6>
+            <small>Please be in the restaurant 5 minutes before.</small>
+        </div>
+    );
+}
+
+export default ReservationDetail;
