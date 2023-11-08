7b7ff6ce9e549e4aaaa10ca3fba652e97f21bff5
tina <tinarezanezhaad@gmail.com>
2023-11-08 21:59:12 +0100

    New endpoint to get reservation and restaurant by id

--- api/api/routes/api.php
+++ api/api/routes/api.php
@@ -10,11 +10,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware(['cors'])->group(function () {
     Route::post('/reservations', [\App\Http\Controllers\ReservationController::class, 'store']);
     Route::get('/reservations', [\App\Http\Controllers\ReservationController::class, 'index']);
+    Route::get('/reservations/{id}', [\App\Http\Controllers\ReservationController::class, 'findOne']);
     Route::put('/reservations/{id}', [\App\Http\Controllers\ReservationController::class, 'update']);
     Route::delete('/reservations/{id}', [\App\Http\Controllers\ReservationController::class, 'delete']);
 
     Route::post('/restaurants', [\App\Http\Controllers\RestaurantController::class, 'store']);
     Route::get('/restaurants', [\App\Http\Controllers\RestaurantController::class, 'index']);
+    Route::get('/restaurants/{id}', [\App\Http\Controllers\RestaurantController::class, 'findOne']);
+
     Route::put('/restaurants/{id}', [\App\Http\Controllers\RestaurantController::class, 'update']);
     Route::delete('/restaurants/{id}', [\App\Http\Controllers\RestaurantController::class, 'delete']);
 });
