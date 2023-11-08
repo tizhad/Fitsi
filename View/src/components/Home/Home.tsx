7dce72a8e17aaa571d96b4169d3cd802d96cb97a
tina <tinarezanezhaad@gmail.com>
2023-11-08 21:59:52 +0100

    Added reservation details

--- View/src/components/Home/Home.tsx
+++ View/src/components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import Modal from "../Modal/Modal";
+import ReservationDetail from "../Reservation/ReservationDetail";
+import {Reservation} from "../../Models/Reservation";
 
 interface HomeProps {
 }
 
 function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [reservation, setReservation] = useState<Reservation>();
 
     function onTableReservation() {
         setIsModalOpen(true);
@@ -15,18 +18,20 @@ function Home() {
         setIsModalOpen(false);
     }
 
-    const onAddRestaurant = () => {
+    const handleReservation = (reserved: Reservation) => {
+        setReservation(reserved);
+        setIsModalOpen(false);
+    };
 
-    }
 
     return (
         <div>
-            <button type="button" className="btn btn-primary" onClick={onTableReservation}>
+            <button type="button" className="btn btn-primary m-2" onClick={onTableReservation}>
                 Reserve a Table
             </button>
-            {isModalOpen && <Modal onClose={closeModal}/>}
-            <button className="btn btn-outline-primary" onClick={onAddRestaurant}>Add Your Restaurant</button>
-
+            {isModalOpen && <Modal onClose={closeModal} onReservation={handleReservation}/>}
+            <button className="btn btn-outline-primary">Add Your Restaurant</button>
+            {reservation &&<ReservationDetail reservation={reservation} />}
         </div>
     );
 }
