d0c7836d34737ba8ad9104849cfd8bffc74d5aeb
tina <tinarezanezhaad@gmail.com>
2023-11-10 16:20:26 +0100

    Cancel reservation btn added.

--- View/src/components/Home/Home.tsx
+++ View/src/components/Home/Home.tsx
@@ -30,9 +30,6 @@ function Home() {
                 <button type="button" className="btn btn-primary m-2" onClick={onTableReservation}>
                     New Reservation
                 </button>
-                <button type="button" className="btn btn-danger m-2" onClick={onTableReservation}>
-                    Cancel Reservation
-                </button>
             </div>)}
             {isModalOpen && <Modal onClose={closeModal} onReservation={handleReservation}/>}
 
